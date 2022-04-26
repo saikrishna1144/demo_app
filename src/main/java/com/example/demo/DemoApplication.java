35470ee79678085c50714f3830b1926605a44c68
saikrishna1144 <msaikrishna.sm@gmail.com>
2022-04-26 13:47:33 +0530

    log adding

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,14 +1,18 @@
 package com.example.demo;
-
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
 @SpringBootApplication
 @RestController
 public class DemoApplication {
+	static Logger log = LogManager.getLogger(DemoApplication.class.getName());  
 	@GetMapping("/message")
     public String message() {
-    	return("demo application");
+		log.debug("Hello this is a debug message");  
+	      log.info("Hello this is an info message"); 
+    	return("demo application second commit");
     }
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
